58e3470faf0278f0aa7bceaaf1e0f1e2ade499b4
Rajesh Paruchuri <rajeshchowdary930@gmail.com>
2025-08-23 23:56:25 -0700

    Add files via upload

---
+++ 23_08.py
@@ -0,0 +1,17 @@
+# Task 1
+a=int(input("Enter your first number : "))
+b=int(input("Enter your second number : "))
+
+print("addition :",a+b)
+print("Subtraction :",a-b)
+print("Multiplication :",a*b)
+print("Division :",a/b)
+
+
+# Task 2
+
+f1=input("Enter your First name ; ")
+f2=input("Enter your Second name ; ")
+
+print(f"Hello ,{f1+f2} ! welcome to the python program")
+
